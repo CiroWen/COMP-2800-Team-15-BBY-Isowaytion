51e7eb98edcd7b7c956eb4820d977c046aaae223
dkeber <44458520+dkeber@users.noreply.github.com>
2020-05-15 13:17:26 -0700

    leaderboard modal will show up for every user.

---
+++ public/leaderboard.js
@@ -0,0 +1,26 @@
+            // Get the content specific to the user.
+            let modal = document.getElementsByClassName("modal");
+
+            // Get the button that opens the modal
+            let btn = document.getElementsByClassName("userDisplay");
+
+            // Get the <span> element that closes the modal
+            let span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal
+            for (let i = 0; i < modal.length; i++) {
+
+              btn[i].onclick = function () {
+                modal[i].style.display = "block";
+              }
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function () {
+                  modal[i].style.display = "none";
+                }
+    
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function () {
+                    modal[i].style.display = "none";
+                }
+            }
