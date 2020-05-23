9b70e20975375be30798460721eeb164b2bea390
HawkanZheng <hawkan.zheng@gmail.com>
2020-05-22 17:42:02 -0700

    Changed urls for local development

--- public/backbutton.js
+++ public/backbutton.js
@@ -2,17 +2,17 @@
 function sendAccount(){
     console.log("Hello");
     //For local development
-    // location.replace("http://localhost:1515/myAccount");
+    location.replace("http://localhost:1515/myAccount");
 
     //Hosted App
-    location.replace("http://isowaytion.com/myAccount");
+    // location.replace("http://isowaytion.com/myAccount");
 }
 
 function sendMap(){
     console.log("Hello");
     //For local development
-    // location.replace("http://localhost:1515/map");
+    location.replace("http://localhost:1515/map");
 
     //Hosted App
-    location.replace("http://isowaytion.com/map");
+    // location.replace("http://isowaytion.com/map");
 }
